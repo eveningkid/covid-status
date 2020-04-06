e115ce451feb071d6aa999e655b5c0685bf44c4c
eveningkid <arn.dellinger@gmail.com>
2020-04-06 19:58:04 +0100

    Add Sentry

--- App.js
+++ App.js
@@ -2,7 +2,15 @@ import React from 'react';
 import { AppearanceProvider } from 'react-native-appearance';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import 'react-native-gesture-handler';
+import * as Sentry from 'sentry-expo';
+import { SENTRY_DSN } from 'react-native-dotenv';
 import LocationsScreen from './src/screens/LocationsScreen';
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+  enableInExpoDevelopment: true,
+  debug: true
+});
 
 export default function App() {
   return (
